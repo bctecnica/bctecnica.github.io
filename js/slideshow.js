b7c699ce74dfde95f109c93185f43c6f7d0078cd
ben <jammydude7@googlemail.com>
2022-05-10 11:17:23 +0100

    added artists gallery to web projects

--- js/slideshow.js
+++ js/slideshow.js
@@ -78,3 +78,30 @@ function showSlides3(n) {
     slides[slideIndex3-1].style.display = "block";
   
 }
+
+// --------Slide4---------
+let slideIndex4 = 1;
+showSlides4(slideIndex4);
+
+// Next/previous controls
+function plusSlides4(n) {
+    showSlides4(slideIndex4 += n);
+}
+  
+  // Thumbnail image controls
+  function currentSlide4(n) {
+    showSlides4(slideIndex4 = n);
+}
+
+function showSlides4(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides4");
+    if (n > slides.length) {slideIndex4 = 1}
+    if (n < 1) {slideIndex4 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+  
+    slides[slideIndex4-1].style.display = "block";
+  
+}
