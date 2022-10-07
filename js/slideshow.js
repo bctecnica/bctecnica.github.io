607ad1dbe61902fd4489197f0002105cf36d0717
ben <jammydude7@googlemail.com>
2022-10-07 19:06:22 +0100

    added Pilates in the park project

--- js/slideshow.js
+++ js/slideshow.js
@@ -105,3 +105,30 @@ function showSlides4(n) {
     slides[slideIndex4-1].style.display = "block";
   
 }
+
+// --------Slide4---------
+let slideIndex5 = 1;
+showSlides5(slideIndex5);
+
+// Next/previous controls
+function plusSlides5(n) {
+    showSlides5(slideIndex5 += n);
+}
+  
+  // Thumbnail image controls
+  function currentSlide5(n) {
+    showSlides5(slideIndex5 = n);
+}
+
+function showSlides5(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides5");
+    if (n > slides.length) {slideIndex5 = 1}
+    if (n < 1) {slideIndex5 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+  
+    slides[slideIndex5-1].style.display = "block";
+  
+}
